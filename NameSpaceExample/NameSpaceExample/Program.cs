761e47b04448be9be760a36f354ae2c58443af88
NPO-NP <78456508+NPO-NP@users.noreply.github.com>
2024-10-11 10:32:43 +0800

    Add files via upload

---
+++ NameSpaceExample/NameSpaceExample/Program.cs
@@ -0,0 +1,38 @@
+﻿
+System.Console.WriteLine("Hello, World!");
+NPO.Output.WriteLine();
+Console.WriteLine(NPO.Output.ave(3, 8));
+ABC.Output.WriteLine();
+
+namespace NPO
+{
+    class Output
+    {
+        public static void WriteLine()
+        {
+            Console.WriteLine("WriteLine method in NPO namespace");
+        }
+
+        public static double ave(int a, int b)
+        {
+            return (a + b) / 2.0;
+        }
+    }
+
+}
+
+namespace ABC
+{
+    class Output
+    {
+        public static void WriteLine()
+        {
+            Console.WriteLine("WriteLine method in ABC namespace");
+        }
+    }
+
+}
+
+
+
+
